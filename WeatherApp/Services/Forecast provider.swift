4a2040c9254bafa9d204cdab55d8d9b4d9eadb0a
Ashish <aashishkc98@gmail.com>
2019-11-02 18:08:11 -0700

    Added JSON and BackUps

---
+++ WeatherApp/Services/Forecast provider.swift
@@ -0,0 +1,9 @@
+//
+//  Forecast provider.swift
+//  WeatherApp
+//
+//  Created by Ashish on 11/2/19.
+//  Copyright © 2019 Greenhouse. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherApp/Services/LocationService.swift
@@ -0,0 +1,13 @@
+//
+//  LocationService.swift
+//  WeatherApp
+//
+//  Created by Ashish on 11/2/19.
+//  Copyright © 2019 Greenhouse. All rights reserved.
+//
+
+import UIKit
+
+class LocationService: NSObject {
+
+}
